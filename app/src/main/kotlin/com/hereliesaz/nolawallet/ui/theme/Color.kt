5b25fedce50c540ed8167cee983cf1bdfe42d72c
Az <103241502+HereLiesAz@users.noreply.github.com>
2025-12-19 05:28:39 -0600

    Create Color.kt

---
+++ app/src/main/kotlin/com/hereliesaz/nolawallet/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.hereliesaz.nolawallet.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val StateBlue = Color(0xFF0F4C81) // Approximately the LA Wallet header blue
+val LicenseOverlayBlue = Color(0xFF1C4670)
+val GoldAccent = Color(0xFFF2C94C)
+val LightGrey = Color(0xFFF5F5F5)
+val DividerGrey = Color(0xFFE0E0E0)
+val TextWhite = Color(0xFFFFFFFF)
+val TextBlack = Color(0xFF000000)
+
+// Card Specific Colors
+val HandgunGold = Color(0xFFFDE68A) // Muted gold for permit
+val VehicleBlue = Color(0xFF42A5F5)
+val HealthGrey = Color(0xFFEEEEEE)
+val LicenseGreen = Color(0xFF7CB342) // LDWF Green
+val TsaBlue = Color(0xFF1565C0)
